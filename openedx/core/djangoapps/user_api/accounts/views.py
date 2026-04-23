5e6a070720ab058f8052ee83e7fb045349387681
ktyagiapphelix2u <ktyagi-apphelix@2u.com>
2026-04-23 10:15:37 +0000

    fix: redact pending primary email before retirement deletion

--- openedx/core/djangoapps/user_api/accounts/views.py
+++ openedx/core/djangoapps/user_api/accounts/views.py
@@ -1151,7 +1151,10 @@ class AccountRetirementView(ViewSet):
 
             self.retire_entitlement_support_detail(user)
 
-            # Retire misc. models that may contain PII of this user
+            # Retire misc. models that may contain PII of this user.
+            # Redact pending email before delete because downstream systems
+            # may preserve soft-deleted snapshots.
+            PendingEmailChange.redact_pending_email_by_user_value(user, field="user")
             PendingEmailChange.delete_by_user_value(user, field="user")
             UserOrgTag.delete_by_user_value(user, field="user")
 
