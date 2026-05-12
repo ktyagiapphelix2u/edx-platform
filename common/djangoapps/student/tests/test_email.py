be853180c26eac40ee1a9dbc95437e6e1bd63af7
ktyagiapphelix2u <ktyagi-apphelix@2u.com>
2026-05-12 06:08:31 +0000

    fix: redact pending primary email before retirement deletion

--- common/djangoapps/student/tests/test_email.py
+++ common/djangoapps/student/tests/test_email.py
@@ -10,10 +10,10 @@ from django.conf import settings
 from django.contrib.auth.models import User  # pylint: disable=imported-auth-user
 from django.core import mail
 from django.db import connection, transaction
-from django.test.utils import CaptureQueriesContext
 from django.http import HttpResponse
 from django.test import TransactionTestCase, override_settings
 from django.test.client import RequestFactory
+from django.test.utils import CaptureQueriesContext
 from django.urls import reverse
 from django.utils.html import escape
 from testfixtures import LogCapture
